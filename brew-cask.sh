536adf4cdc80580e9e3c662f736aa6e81b0692c8
jpsalo <jpsalo@zoho.com>
2017-11-22 09:53:21 +0200

    Added a list of installed homebrew casks

---
+++ brew-cask.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# https://github.com/paulirish/dotfiles/blob/master/brew-cask.sh
+# brew cask list
+brew cask install adobe-digital-editions
+brew cask install amethyst
+brew cask install basecamp
+brew cask install cscreen
+brew cask install deluge
+brew cask install dropbox
+brew cask install duet
+brew cask install fastlane
+brew cask install freedom
+brew cask install google-backup-and-sync
+brew cask install google-chrome-canary
+brew cask install google-drive
+brew cask install graphiql
+brew cask install messenger
+brew cask install noti
+brew cask install owasp-zap
+brew cask install postman
+brew cask install react-native-debugger
+brew cask install soundnode
+brew cask install staruml
+brew cask install toggldesktop
+brew cask install tunnelblick
+brew cask install virtualbox
+brew cask install vitalsource-bookshelf
